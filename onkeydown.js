e33cdabb0b5288b5f5f0f8bb0d42d280cc812970
bushbushsaur <56460158+bushbushsaur@users.noreply.github.com>
2021-01-19 10:50:54 -0800

    Update onkeydown.js

--- onkeydown.js
+++ onkeydown.js
@@ -3,7 +3,7 @@ window.focus();
 document.onkeydown = function(e) {
     var $a = document.querySelectorAll("a");
     for (var i = 0;i < $a.length;i++) {
-        if (e.code == $a.getAttribute("code")) {
+        if (e.key == $a.getAttribute("key")) {
             window.location.href = $a.getAttribute("href");
         }
     }
